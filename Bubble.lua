2441b011030938c8d6d7483d11a9eac5757dd63e
Arne De Herdt <arne.de.herdt@gmail.com>
2015-04-19 15:17:31 +0200

    Trying to catch more events

--- Bubble.lua
+++ Bubble.lua
@@ -4,6 +4,7 @@
 -----------------------------------------------------------------------------------------------
  
 require "Window"
+require "Unit"
  
 -----------------------------------------------------------------------------------------------
 -- Bubble Module Definition
@@ -24,68 +25,59 @@ function Bubble:new(o)
     self.__index = self 
 
     -- initialize variables here
+	self.tUnits = {}
 
     return o
 end
 
-function Bubble:Init()
-	local bHasConfigureFunction = false
-	local strConfigureButtonText = ""
-	local tDependencies = {
-		-- "UnitOrPackageName",
-	}
-    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
-end
- 
-
------------------------------------------------------------------------------------------------
--- Bubble OnLoad
------------------------------------------------------------------------------------------------
-function Bubble:OnLoad()
-    -- load our form file
-	self.xmlDoc = XmlDoc.CreateFromFile("Bubble.xml")
-	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
-end
-
------------------------------------------------------------------------------------------------
--- Bubble OnDocLoaded
------------------------------------------------------------------------------------------------
-function Bubble:OnDocLoaded()
-
-	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
-	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "BubbleForm", nil, self)
-		if self.wndMain == nil then
-			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
-			return
-		end
-		
-	    self.wndMain:Show(false, true)
-
-		-- if the xmlDoc is no longer needed, you should set it to nil
-		self.xmlDoc = nil
-		
-		-- Register handlers for events, slash commands and timer, etc.
-		Apollo.RegisterEventHandler("ChatMessage", "OnChatMessage", self)
-	end
+function Bubble:Init()	
+    Apollo.RegisterAddon(self)
+	Apollo.RegisterEventHandler("ChatMessage", "OnChatMessage", self)
+	Apollo.RegisterEventHandler("UnitCreated", "OnUnitCreated", self)
+	Apollo.RegisterEventHandler("UnitDestroyed", "OnUnitDestroyed", self)
 end
 
 -----------------------------------------------------------------------------------------------
 -- Bubble Functions
 -----------------------------------------------------------------------------------------------
+
+-- Get's triggered whenever a Unit is created. When the unit is created, we store that
+-- unit, along with it's ID in our units table to track it for proper name displays.
+function Bubble:OnUnitCreated(unitSpawned)
+	table.insert(self.tUnits, { id = unitSpawned:GetId(), unit = unitSpawned })
+end
+
+-- Get's triggered whenever a Unit is despawned.
+-- We remove the unit from our table.
+function Bubble:OnUnitDestroyed(unitDestroyed)
+	for idx,tUnit in ipairs(self.tUnits) do
+		if(tUnit.id == unitDestroyed:GetId()) then
+			self.tUnits[unitSpawned:GetId()] = nil
+		end
+	end
+end
+
+-- Displays messages that are being said using /say in the bubbles above people's head.
+-- If the unit cannot be found, then we do not anything.
 function Bubble:OnChatMessage(channelCurrent, tMessage)
-	local unitSender = tMessage.unitSource
-	
-	if unitSender == nil then return end
-	
+	local unitSender = tMessage.unitSource	
 	local strMessage = ""
 	
 	for idx, tSegment in ipairs(tMessage.arMessageSegments) do
-		strMessage = strMessage ..tSegment.strText
+		strMessage = strMessage .. tSegment.strText
 	end
-		
-	unitSender:AddTextBubble(strMessage)
+	
+	-- No unit provided, could be a channel message, let's see if the Unit is nearby
+	if unitSender == nil then
+		for idx,tUnit in ipairs(self.tUnits) do
+			if(tUnit.unit:GetName() == tMessage.strSender) then
+				tUnit.unit:AddTextBubble(strMessage)
+			end
+		end
+	else
+		unitSender:AddTextBubble(strMessage)
+	end
 end
-
 -----------------------------------------------------------------------------------------------
 -- Bubble Instance
 -----------------------------------------------------------------------------------------------

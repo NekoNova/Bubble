caee7b2e41a872476d7dba9a11b94e914170dcdb
Arne De Herdt <arne.de.herdt@maxdome.de>
2015-04-21 13:44:03 +0200

    v1.3 : added experimental /laugh support

--- Bubble.lua
+++ Bubble.lua
@@ -1,10 +1,9 @@
------------------------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------------------------
 -- Client Lua Script for Bubble
 -- Copyright (c) NCsoft. All rights reserved
 -----------------------------------------------------------------------------------------------
  
 require "Window"
-require "Unit"
  
 -----------------------------------------------------------------------------------------------
 -- Bubble Module Definition
@@ -14,7 +13,7 @@ local Bubble = {}
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
--- e.g. local kiExampleVariableMax = 999
+local karLaughKeyWords = { "laugh", "laughing", "lol", "heh", "hehe" }
  
 -----------------------------------------------------------------------------------------------
 -- Initialization
@@ -25,59 +24,131 @@ function Bubble:new(o)
     self.__index = self 
 
     -- initialize variables here
-	self.tUnits = {}
 
     return o
 end
 
-function Bubble:Init()	
-    Apollo.RegisterAddon(self)
-	Apollo.RegisterEventHandler("ChatMessage", "OnChatMessage", self)
-	Apollo.RegisterEventHandler("UnitCreated", "OnUnitCreated", self)
-	Apollo.RegisterEventHandler("UnitDestroyed", "OnUnitDestroyed", self)
+function Bubble:Init()
+	local bHasConfigureFunction = false
+	local strConfigureButtonText = ""
+	local tDependencies = {
+		-- "UnitOrPackageName",
+	}
+    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
+end
+ 
+
+-----------------------------------------------------------------------------------------------
+-- Bubble OnLoad
+-----------------------------------------------------------------------------------------------
+function Bubble:OnLoad()
+    -- load our form file
+	self.xmlDoc = XmlDoc.CreateFromFile("Bubble.xml")
+	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
+end
+
+-----------------------------------------------------------------------------------------------
+-- Bubble OnDocLoaded
+-----------------------------------------------------------------------------------------------
+-- Gets called when the XML document finishes loading.
+-- Although we do not have any form to load, we're keeping the code, as we might want
+-- to be able to configure something in the future.
+function Bubble:OnDocLoaded()
+	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
+    self.wndMain = Apollo.LoadForm(self.xmlDoc, "BubbleForm", nil, self)
+	
+		if self.wndMain == nil then
+			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
+			return
+		end
+		
+    self.wndMain:Show(false, true)
+
+		-- if the xmlDoc is no longer needed, you should set it to nil
+		self.xmlDoc = nil
+		
+		-- Register handlers for events, slash commands and timer, etc.
+		Apollo.RegisterEventHandler("ChatMessage", "OnChatMessage", self)
+	end
 end
 
 -----------------------------------------------------------------------------------------------
 -- Bubble Functions
 -----------------------------------------------------------------------------------------------
-
--- Get's triggered whenever a Unit is created. When the unit is created, we store that
--- unit, along with it's ID in our units table to track it for proper name displays.
-function Bubble:OnUnitCreated(unitSpawned)
-	table.insert(self.tUnits, { id = unitSpawned:GetId(), unit = unitSpawned })
-end
-
--- Get's triggered whenever a Unit is despawned.
--- We remove the unit from our table.
-function Bubble:OnUnitDestroyed(unitDestroyed)
-	for idx,tUnit in ipairs(self.tUnits) do
-		if(tUnit.id == unitDestroyed:GetId()) then
-			self.tUnits[unitSpawned:GetId()] = nil
-		end
-	end
-end
-
--- Displays messages that are being said using /say in the bubbles above people's head.
--- If the unit cannot be found, then we do not anything.
 function Bubble:OnChatMessage(channelCurrent, tMessage)
-	local unitSender = tMessage.unitSource	
+  -- Information:
+  --
+  -- tMessage contains the following properties:
+  --
+  --  bAutoResponse     : Boolean indicating whether this message is an automated response.
+  --  bGM               : Boolean indicating whether this message comes from a GM
+  --  bSelf             : Boolean indicating if this is a message the player wrote
+  --  strSender         : The name of the sender
+  --  strRealmName      : The name of the Realm
+  --  nPresenceState    : 0 on successful message
+  --  arMessageSegments : The segments of the message.
+  --  unitSource        : the unit sending the message.
+  --  bShowChatBubble   : Boolean indicating whether to show the bubble.
+  --  bCrossFaction     : Boolean indicating whether this is cross faction.
+  --  nReportId         : The ID for reporting the message.
+  --
+  -- arMessageSegments contains the following structure:
+  --
+  --  strText     : The message
+  --  bRolePlay   : Boolean if this is a RP message
+  --  bAlien      : Boolean if this is from the other faction
+  --  bProfanity  : Boolean for swearing
+  --
+	if tMessage.unitSource == nil then return end
+	
 	local strMessage = ""
+	local channel = channelCurrent:GetType()
 	
 	for idx, tSegment in ipairs(tMessage.arMessageSegments) do
-		strMessage = strMessage .. tSegment.strText
+		strMessage = strMessage ..tSegment.strText
 	end
 	
-	-- No unit provided, could be a channel message, let's see if the Unit is nearby
-	if unitSender == nil then
-		for idx,tUnit in ipairs(self.tUnits) do
-			if(tUnit.unit:GetName() == tMessage.strSender) then
-				tUnit.unit:AddTextBubble(strMessage)
-			end
-		end
-	else
-		unitSender:AddTextBubble(strMessage)
+	-- Check for emotes, we're going to surrounded them by *
+	if channel == ChatSystemLib.ChatChannel_AnimatedEmote or channel == ChatSystemLib.ChatChannel_Emote then
+		strMessage = "*"..strMessage.."*"
 	end
+	
+	-- If we're dealing with a Yell, then we'll add some exclamation marks
+	if channel == ChatSystemLib.ChatChannel_Yell then
+		strMessage = strMessage.."!!"
+	end
+		
+	-- Check all required Emotes, and fire them before displaying the text bubble
+	if(tMessage.unitSource == GameLib.GetPlayerUnit()) then
+    self:HelperLaughingEmote(strMessage)
+	end
+	
+	-- Display the text-bubble using correct formatting.
+	self:DisplayBubble(tMessage.unitSource, strMessage)
 end
+
+-- Checks whether the provided text contains the required keys that would
+-- trigger our laughing Emote, and actually invoke the Emote based on that.
+-- We only trigger the emote on the first match
+function Bubble:HelperLaughingEmote(strMessage)
+  for i = 1, #karLaughKeyWords do
+    local strPattern = "*"..karLaughKeyWords[i].."*"
+    
+    if(strMessage.match(pattern)) then
+      ChatSystemLib.Command("/laugh")
+      break
+    end
+  end
+end
+
+-- Displays the chat bubble on the provided unitTarget, and injects the
+-- provided strMessage inside the text bubble.
+-- TODO: Perform proper formatting
+--
+function Bubble:DisplayBubble(unitTarget, strMessage)
+  unitTarget:AddTextBubble(strMessage)
+end
+
 -----------------------------------------------------------------------------------------------
 -- Bubble Instance
 -----------------------------------------------------------------------------------------------

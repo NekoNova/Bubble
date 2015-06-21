1e01db682ec174d6d42270ce97dd9f25e9be4c31
Arne De Herdt <arne.de.herdt@gmail.com>
2015-06-21 19:02:19 +0200

    v1.6 : Party Chat

--- Bubble.lua
+++ Bubble.lua
@@ -23,12 +23,14 @@ function Bubble:new(o)
     self.__index = self 
 
     -- initialize variables here
-	o.tSettings = {
+	self.tSettings = {
 		nTimeout = 5,
-		arrKeywords = { "laugh", "laughing", "lol", "heh", "hehe" }
+		arrKeywords = { "laugh", "laughing", "lol", "heh", "hehe" },
+		bPartyChat = false,
+		bYellEmote = false,
 	}
-	o.nSayCounter = 0
-	o.nLaughCounter = 0
+	
+	self.nSayCounter = 0
 	
     return o
 end
@@ -90,10 +92,8 @@ function Bubble:OnSave(eLevel)
 	-- Create a table to store our data.
 	local tSave = {}
 	
-	tSave.nLaughCounter = self.nLaughCounter
 	tSave.nSayCounter = self.nSayCounter
-	tSave.nTimeout = self.nTimeout
-	tSave.arrKeywords = self.arrKeywords
+	tSave.tSettings = self.tSettings
 	
 	-- Simply return the data
 	return tSave
@@ -104,10 +104,8 @@ function Bubble:OnRestore(eLevel, tData)
 		return
 	end
 	
-	self.nLaughCounter = tData.nLaughCounter
 	self.nSayCounter = tData.nSayCounter
-	self.nTimeout = tData.nTimeout
-	self.arrKeywords = tData.arrKeywords
+	self.tSettings = tData.tSettings
 end
 -----------------------------------------------------------------------------------------------
 -- Bubble Functions
@@ -119,6 +117,8 @@ function Bubble:OnBubbleSlashCommand(strCommand, strArgs)
 	-- Load all settings
 	self.wndMain:FindChild("input_s_Timeout"):SetText(self.tSettings.nTimeout)
 	self.wndMain:FindChild("input_s_Keywords"):SetText(table.concat(self.tSettings.arrKeywords, ","))
+	self.wndMain:FindChild("btnPartyChat"):SetCheck(self.tSettings.bPartyChat)
+	self.wndMain:FindChild("btnYellEmote"):SetCheck(self.tSettings.bYellEmote)
 	self.wndMain:Show(true)
 end
 
@@ -146,10 +146,10 @@ function Bubble:OnChatMessage(channelCurrent, tMessage)
 	--  bAlien      : Boolean if this is from the other faction
 	--  bProfanity  : Boolean for swearing
 	--
-	if tMessage.unitSource == nil then return end
-	
 	local strMessage = ""
 	local channel = channelCurrent:GetType()
+	
+	if(tMessage.unitSource == nil and channel ~= ChatSystemLib.ChatChannel_Party) then return end	
 
 	-- Break out of emotes, to prevent the channel from being spammed.
 	if channel == ChatSystemLib.ChatChannel_AnimatedEmote or channel == ChatSystemLib.ChatChannel_Emote then
@@ -160,20 +160,26 @@ function Bubble:OnChatMessage(channelCurrent, tMessage)
 	for idx, tSegment in ipairs(tMessage.arMessageSegments) do
     	strMessage = strMessage ..tSegment.strText
 	end
-	
-	-- If we're dealing with a Yell, then we'll add some exclamation marks
-	if channel == ChatSystemLib.ChatChannel_Yell then
-    	strMessage = strMessage.."!!"
-	end
 		
 	-- Check all required Emotes, and fire them before displaying the text bubble
-	if(tMessage.unitSource == GameLib.GetPlayerUnit()) then
+	if(tMessage.bSelf) then
     	self:HelperLaughingEmote(strMessage)
 	    self:HelperSayEmote(channelCurrent)
+		self:HelperShoutEmote(channelCurrent)
+		self:DisplayBubble(GameLib.GetPlayerUnit(), strMessage)
+	elseif(tMessage.unitSource ~= nil) then
+		self:DisplayBubble(tMessage.unitSource, strMessage)
+	else
+		return
 	end
-	
-	-- Display the text-bubble using correct formatting.
-	self:DisplayBubble(tMessage.unitSource, strMessage)
+end
+
+-- Checks whether the received channel is currently /yell, and fires the
+-- /shout command so the character performs a real shout.
+function Bubble:HelperShoutEmote(channel)
+	if(channel:GetType() == ChatSystemLib.ChatChannel_Yell and self.tSettings.bYellEmote) then
+		ChatSystemLib.Command("/shout")
+	end
 end
 
 -- Checks whether the provided text contains the required keys that would
@@ -184,13 +190,7 @@ function Bubble:HelperLaughingEmote(strMessage)
     	local strPattern = ".*"..karLaughKeyWords[i]..".*"
     
 	    if(strMessage:match(strPattern)) then
-			if (self.nLaughCounter >= self.tSettings.nTimeout) then
-				self.nLaughCounter = 1
-				ChatSystemLib.Command("/laugh")
-				return
-			else
-				self.nLaughCounter = self.nLaughCounter + 1
-			end
+			ChatSystemLib.Command("/laugh")			
 	    end
 	end
 end
@@ -198,7 +198,7 @@ end
 -- Checks whether the received channel is currently say, and fires the
 -- /talk command so the character performs the chat emote when saying something.
 function Bubble:HelperSayEmote(channel)
-	if channel:GetType() == ChatSystemLib.ChatChannel_Say then
+	if(channel:GetType() == ChatSystemLib.ChatChannel_Say or (channel:GetType() == ChatSystemLib.ChatChannel_Party and self.tSettings.bPartyChat)) then
 		if(self.nSayCounter >= self.tSettings.nTimeout) then
 			self.nSayCounter = 1
 			ChatSystemLib.Command("/talk")
@@ -212,7 +212,7 @@ end
 -- provided strMessage inside the text bubble.
 -- TODO: Perform proper formatting
 function Bubble:DisplayBubble(unitTarget, strMessage)
-	  unitTarget:AddTextBubble(strMessage)
+	unitTarget:AddTextBubble(strMessage)
 end
 
 -- Returns the chatChannel by the given Name.
@@ -228,8 +228,11 @@ end
 function Bubble:OnOK( wndHandler, wndControl, eMouseButton )
 	local nTimeout = self.wndMain:FindChild("input_s_Timeout"):GetText()
 	local arrKeywords = self.wndMain:FindChild("input_s_Keywords"):GetText()
-	
-	self.tSettings.arrKeywords = {}
+
+	-- Clear our table properly
+	for k in pairs (self.tSettings.arrKeywords) do
+		self.tSettings.arrKeywords[k] = nil
+	end
 	
 	-- Iterate over the string in the keyword box, split it by ,
 	-- and store each keyword string inside the settings array.
@@ -239,12 +242,27 @@ function Bubble:OnOK( wndHandler, wndControl, eMouseButton )
 	
 	-- Store the Data
 	self.tSettings.nTimeout = tonumber(nTimeout) or 5
+	self.tSettings.bPartyChat = self.wndMain:FindChild("btnPartyChat"):IsChecked()
+	self.tSettings.bYellEmote = self.wndMain:FindChild("btnYellEmote"):IsChecked()
 	
 	Print("Bubble:New Settings Saved!")
 end
 
 function Bubble:OnCancel( wndHandler, wndControl, eMouseButton )
 	self.wndMain:Show(false)
+end
+
+---------------------------------------------------------------------------------------------------
+-- BubbleForm Functions
+---------------------------------------------------------------------------------------------------
+
+-- Gets triggered when the PartyChatCheckbox gets toggled.
+function Bubble:OnPartyChatToggle( wndHandler, wndControl, eMouseButton )
+	self.tSettings.bPartyChat = wndControl:IsChecked()
+end
+
+function Bubble:OnBtnYellEmoteToggle( wndHandler, wndControl, eMouseButton )
+	self.tSettings.bYellEmote = wndControl:IsChecked()
 end
 
 -----------------------------------------------------------------------------------------------
